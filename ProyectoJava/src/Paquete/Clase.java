728e62bf9730881dbda0361d101d3c0b26351cad
jzldegit <jzldegit@gmail.com>
2026-03-05 12:41:55 +0100

    Nueva rama

--- ProyectoJava/src/Paquete/Clase.java
+++ ProyectoJava/src/Paquete/Clase.java
@@ -4,7 +4,8 @@ public class Clase {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!");
-
+		System.out.println("Que bonito este mundo!");
+		System.out.println("Adios Mundo!");
 	}
 
 }
